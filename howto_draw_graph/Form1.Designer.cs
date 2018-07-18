7c55f4da9b5b6f7f3b3c173dbde3ed34252cc35f
hmaster20 <hmaster20@gmail.com>
2018-07-18 21:35:56 +0300

    add tooltip

---
+++ howto_draw_graph/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace howto_draw_graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picGraph = new System.Windows.Forms.PictureBox();
+            this.tipData = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGraph
+            // 
+            this.picGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGraph.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picGraph.Location = new System.Drawing.Point(12, 12);
+            this.picGraph.Name = "picGraph";
+            this.picGraph.Size = new System.Drawing.Size(581, 349);
+            this.picGraph.TabIndex = 0;
+            this.picGraph.TabStop = false;
+            this.picGraph.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picGraph_MouseMove);
+            this.picGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.picGraph_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 373);
+            this.Controls.Add(this.picGraph);
+            this.Name = "Form1";
+            this.Text = "howto_draw_graph";
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picGraph;
+        private System.Windows.Forms.ToolTip tipData;
+    }
+}
+
